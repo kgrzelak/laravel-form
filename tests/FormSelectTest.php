86b6fb19dbc58c1b0265a15b98542bd60b39fdb9
Krzysztof Grzelak <krzysztof.grzelak21@gmail.com>
2024-06-29 19:50:38 +0200

    Form select update

---
+++ tests/FormSelectTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kgrzelak\LaravelForm\Tests;
+
+use Kgrzelak\LaravelForm\LaravelForm;
+
+class FormSelectTest extends TestCase
+{
+    public function testCanRenderSelect(): void
+    {
+        $select = LaravelForm::select()->name('test');
+
+        $this->assertEquals('<select name="test" class="form-select"></select>', $select);
+    }
+
+    public function testCanAddOptionToSelect(): void
+    {
+        $select = LaravelForm::select()->name('test')->addOption('test', 'Test');
+
+        $this->assertEquals('<select name="test" class="form-select"><option value="test">Test</option></select>', $select);
+    }
+
+    public function testCanAddMultipleOptionsToSelect(): void
+    {
+        $select = LaravelForm::select()->name('test')->addOption('test', 'Test')->addOption('test2', 'Test 2');
+
+        $this->assertEquals('<select name="test" class="form-select"><option value="test">Test</option><option value="test2">Test 2</option></select>', $select);
+    }
+}
